619f70195bb9adce15dfa88c4ddd953f4520defa
raushan <roushancri@gmail.com>
2020-11-30 12:47:04 +0530

    frame added

--- src/com/ChattingApp/Server.java
+++ src/com/ChattingApp/Server.java
@@ -1,5 +1,15 @@
 package com.ChattingApp;
 
-public class Server {
+import javax.swing.*;
+
+public class Server extends JFrame{
+    Server(){
+        setSize(450,700);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Server().setVisible(true);
+    }
 
 }
